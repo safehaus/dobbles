ce19f0b705ef5e25a431d96204715686d13188e6
Calvin Hopkins <thechopkins@outlook.com>
2015-05-22 08:49:08 -0700

    Adding untracked file

---
+++ Contract/Shared/IBackgroundService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Safehaus.IntranetGaming.Contract.Shared
+{
+    public interface IBackgroundService
+    {
+        Task Run();
+    }
+}
