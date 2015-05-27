2bee5d93b7f300b407ed58c895bd3fb779e43f63
Josh Lory <josh.lory@code.org>
2015-05-26 23:41:52 -0700

    Add react skeleton

---
+++ IntranetGaming/Web/assets/js/app.js
@@ -0,0 +1,11 @@
+﻿// Main app JS.
+
+var Fibbage = React.createClass({
+  render: function () {
+    return <div>Hello world</div>;
+  }
+});
+
+(function () {
+  React.render(React.createElement(Fibbage, {}), document.querySelector('#reactify'));
+})();
