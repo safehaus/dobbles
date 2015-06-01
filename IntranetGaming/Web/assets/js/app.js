a0e1f627a1609dcb7848a07eeb28242bd9698ba1
Josh Lory <josh.lory@code.org>
2015-05-31 17:25:02 -0700

    UI for joining a room

--- IntranetGaming/Web/assets/js/app.js
+++ IntranetGaming/Web/assets/js/app.js
@@ -2,7 +2,39 @@
 
 var Fibbage = React.createClass({
   render: function () {
-    return <div>Hello world</div>;
+    return (
+      <JoinRoom/>
+    );
+  }
+});
+
+var JoinRoom = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <TextInput label='Room'/>
+        <TextInput label='Your Name' go='Join'/>
+      </div>
+    );
+  }
+});
+
+var TextInput = React.createClass({
+  propTypes: {
+    label: React.PropTypes.string.isRequired,
+    go: React.PropTypes.string
+  },
+
+  render: function () {
+    return (
+      <div>
+        <label>
+          {this.props.label}
+          <input type='text'/>
+        </label>
+        {this.props.go ? <button>{this.props.go}</button> : ''}
+      </div>
+    );
   }
 });
 
