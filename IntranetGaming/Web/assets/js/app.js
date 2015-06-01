525bad47096764d285dd1d497795a66abceb62f0
Josh Lory <josh.lory@code.org>
2015-05-31 18:15:21 -0700

    Add mock API that saves state locally for now

--- IntranetGaming/Web/assets/js/app.js
+++ IntranetGaming/Web/assets/js/app.js
@@ -1,5 +1,20 @@
 ﻿// Main app JS.
 
+var api = (function () {
+
+  var data = {};
+
+  return {
+    room: {
+      join: function (name, roomCode) {
+        data.name = name;
+        data.roomCode = roomCode;
+      }
+    },
+    data: data
+  }
+})();
+
 var Fibbage = React.createClass({
   render: function () {
     return (
@@ -10,10 +25,14 @@ var Fibbage = React.createClass({
 
 var JoinRoom = React.createClass({
   render: function () {
+    var join = function () {
+      api.room.join(this.refs.name.getText(), this.refs.room.getText());
+    }.bind(this);
+
     return (
       <div>
-        <TextInput label='Room'/>
-        <TextInput label='Your Name' go='Join'/>
+        <TextInput label='Room' ref='room'/>
+        <TextInput label='Your Name' go='Join' action={join} ref='name'/>
       </div>
     );
   }
@@ -22,7 +41,12 @@ var JoinRoom = React.createClass({
 var TextInput = React.createClass({
   propTypes: {
     label: React.PropTypes.string.isRequired,
-    go: React.PropTypes.string
+    go: React.PropTypes.string,
+    action: React.PropTypes.func
+  },
+
+  getText: function () {
+    return React.findDOMNode(this.refs.text).value;
   },
 
   render: function () {
@@ -30,9 +54,9 @@ var TextInput = React.createClass({
       <div>
         <label>
           {this.props.label}
-          <input type='text'/>
+          <input type='text' ref='text'/>
         </label>
-        {this.props.go ? <button>{this.props.go}</button> : ''}
+        {this.props.go ? <button onClick={this.props.action}>{this.props.go}</button> : ''}
       </div>
     );
   }
