07d056df8cfa4dda801b44ef23f4d3a2dbafdfe8
Josh Lory <josh.lory@code.org>
2015-05-31 20:46:34 -0700

    UI for waiting for players

--- IntranetGaming/Web/assets/js/app.js
+++ IntranetGaming/Web/assets/js/app.js
@@ -16,23 +16,64 @@ var api = (function () {
 })();
 
 var Fibbage = React.createClass({
+
+  getInitialState: function () {
+    var handleJoin = function () {
+      this.setState({
+        screen: WaitingForPlayers,
+        props: {
+          // TODO: get from service (call handleJoin with data)
+          playerCount: 1
+        }
+      });
+    }.bind(this);
+
+    return {
+      screen: JoinRoom,
+      props: {
+        onJoin: handleJoin
+      }
+    };
+  },
+
   render: function () {
-    return (
-      <JoinRoom/>
-    );
+    return React.createElement(this.state.screen, this.state.props);
   }
 });
 
 var JoinRoom = React.createClass({
+  propTypes: {
+    onJoin: React.PropTypes.func.isRequired
+  },
+
   render: function () {
     var join = function () {
       api.room.join(this.refs.name.getText(), this.refs.room.getText());
+      // TODO: iff successful
+      this.props.onJoin();
     }.bind(this);
 
     return (
       <div>
+        <h2>Join a room</h2>
         <TextInput label='Room' ref='room'/>
         <TextInput label='Your Name' go='Join' action={join} ref='name'/>
+      </div>
+    );
+  }
+});
+
+var WaitingForPlayers = React.createClass({
+  propTypes: {
+    playerCount: React.PropTypes.number.isRequired
+  },
+
+  render: function () {
+    return (
+      <div>
+        <h2>Waiting for players</h2>
+        <p>{this.props.playerCount} joined</p>
+        <button>Everyone's here</button>
       </div>
     );
   }
